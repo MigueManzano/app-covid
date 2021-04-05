94d6b3b9cb6a53cbb5a78c9457cf359232c3af75
Miguel Gutierrez <ing.dev.manzano@gmail.com>
2021-04-04 21:10:55 -0500

    comentarios de la conexion a la BD

--- db/connection.php
+++ db/connection.php
@@ -1,12 +1,16 @@
 <?php
 
+//variables de la cadena de conexion a la BD
 $servidor = "127.0.0.1";
 $nombreusuario = "root";
 $password = "cardenas1501993";
 $db = "sistema_ldc";
 
+//cadena de conexion a la BD
 $conexion = new mysqli($servidor, $nombreusuario, $password, $db);
 
+//condicional para corroborar que la conexion a sido correcta de caso contrario nos manda un error
+//de que la conexion a sido fallida
 if($conexion){
     echo "conexion correcta...";
 }else{
