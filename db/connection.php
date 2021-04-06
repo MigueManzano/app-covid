f543d2d841ce5bce85776bb32c394d1d72858594
KevinQuevedo98 <kevinquevedo1998@outlook.es>
2021-04-05 21:32:21 -0500

    PDO

--- db/connection.php
+++ db/connection.php
@@ -3,11 +3,13 @@
 //variables de la cadena de conexion a la BD
 $servidor = "127.0.0.1";
 $nombreusuario = "root";
-$password = "cardenas1501993";
+///$password = "cardenas1501993"; para migue
+$password ="";
 $db = "sistema_ldc";
 
 //cadena de conexion a la BD
-$conexion = new mysqli($servidor, $nombreusuario, $password, $db);
+///$conexion = new mysqli($servidor, $nombreusuario, $password, $db);
+$conexion = new PDO("mysql:host=$servidor;dbname=$db", $nombreusuario, $password);
 
 //condicional para corroborar que la conexion a sido correcta de caso contrario nos manda un error
 //de que la conexion a sido fallida
